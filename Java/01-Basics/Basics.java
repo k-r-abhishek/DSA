7fba3951e1fe2af227f4f8396f6394b40dc87e94
k-r-abhishek <abhishekkrcd@gmail.com>
2025-03-27 14:03:57 +0530

    basics of java

--- Java/01-Basics/Basics.java
+++ Java/01-Basics/Basics.java
@@ -1,6 +1,7 @@
 /*
  * 1. Basics: Hello World and Variables
  */
+
 public class Basics {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); // Printing Output
